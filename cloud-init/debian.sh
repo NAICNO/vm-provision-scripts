9157f6b3009db4b24f64c702a3205ed6d7e832c1
Ashen Randika <ashenr@users.noreply.github.com>
2024-03-18 15:15:37 +0100

    Initialing EESSI for the first time after the CVMFS setup. (#2)
    Adding checks and waiting until the CVMFS downloads the init script for the first time.

--- cloud-init/debian.sh
+++ cloud-init/debian.sh
@@ -18,3 +18,18 @@ sudo bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
 
 # make sure that EESSI CernVM-FS repository is accessible
 sudo cvmfs_config setup
+
+# Wait for the file to become available
+timeout=30 # Timeout in seconds
+elapsed=0
+
+while [ ! -f /cvmfs/software.eessi.io/versions/2023.06/init/bash ]; do
+  sleep 1
+  ((elapsed++))
+  if [ "$elapsed" -ge "$timeout" ]; then
+    exit 1
+  fi
+done
+
+# Source the EESSI init script
+source /cvmfs/software.eessi.io/versions/2023.06/init/bash
